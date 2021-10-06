e90388beb997204e7883813eb7f5e7c7dd1fa437
Devang <papinwardevang@gmail.com>
2021-10-06 08:22:17 +0530

    Initial Commit

---
+++ Bankers Algo/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Bankers Algo
+//
+//  Created by Devang Papinwar on 06/10/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
